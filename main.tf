4bc67c2388c6c59fbc0d79858f52b12946528e33
Manju M J <manjuan@google.com>
2023-01-03 19:05:09 +0530

    new changes added

--- main.tf
+++ main.tf
@@ -14,40 +14,6 @@
  * limitations under the License.
  */
 
-variable "zone" {
-  type = string
-}
-
-variable "project_id" {
-  type = string
-}
-
-variable "project_number" {
-  type = string
-}
-
-variable "basename" {
-  type = string
-}
-
-variable "region" {
-  type = string
-}
-
-variable "username" {
-  type = string
-}
-
-variable "privatekeypath" {
-    type = string
-    default = "./id_rsa"
-}
-
-variable "publickeypath" {
-    type = string
-    default = "./id_rsa.pub"
-}
-
 locals {
   sacompute = "${var.project_number}-compute@developer.gserviceaccount.com"
 }
